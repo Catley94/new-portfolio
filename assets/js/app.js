f56524c7ffd6bd5f884d06b04e91a2b940027eaf
positive-season <scp94373@gmail.com>
2022-06-03 20:21:52 +0100

    Added my projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,76 @@
 const projects = [
     {
-        Title: "Inverted Wings",
-        ShortDesc: "Music Visualiser",
-        Thumbnail: "",
-        LibrariesUsed: "P5.JS",
-        LongDesc: "Inverted Wings use lines grounded on a pulsing circle. Bass circles will omit once volume exceeds certain amount. Made with P5.js.",
+        title: "Inverted Wings",
+        shortDesc: "Music Visualiser",
+        thumbnail: "",
+        librariesUsed: "P5.JS",
+        longDesc: "Inverted Wings use lines grounded on a pulsing circle. Bass circles will omit once volume exceeds certain amount. Made with P5.js.",
         showOff: true
     },
     {
-        Title: "Text adventure template",
-        ShortDesc: "Template for creating text adventure games",
-        Thumbnail: "",
-        LibrariesUsed: "",
-        LongDesc: "Text adventure template is exactly that, a template that allows others to edit and create their own two option text adventure game. This was originally created for covid lockdown for creativity, it's made for simplicity, everything is dynamic, all you need to do is add the state onto the storyBook object and the rest will happen like magic!",
+        title: "Text adventure template",
+        shortDesc: "Template for creating text adventure games",
+        thumbnail: "",
+        librariesUsed: "",
+        longDesc: "Text adventure template is exactly that, a template that allows others to edit and create their own two option text adventure game. This was originally created for covid lockdown for creativity, it's made for simplicity, everything is dynamic, all you need to do is add the state onto the storyBook object and the rest will happen like magic!",
         showOff: false
+    },
+    {
+        title: "AskSam",
+        shortDesc: "Ask a random question and get an answer!",
+        thumbnail: "",
+        librariesUsed: "NodeJS, Express, MongoDB, CookieParser, Vue",
+        longDesc: `
+            This is a private repo, in order to view the code, you must ask for permission by contacting me. <br/><br/>
+            AskSam is an SPA built with Vue (and Axios) on the Frontend and NodeJS on the backend.
+            The purpose is for anyone to submit a question on the front facing SPA (the user can submit as many questions as they like in one session or multiple), which will both create a cookie on the user's browser with the questionID (so it is able to keep track if the question has been answered) and submits the question to the database (MongoDB). There is also another SPA for 'internal' or 'staff' use, on this SPA it will display all the unanswered questions in a list, the 'staff' member can click on any unanswered question (sorted from oldest at the top, newest at the bottom) and answer it, once answered it will update the database to mark this question as answered. When the user visits the website, it will check the database with the questionID in their cookie, if the question has been answered, it will update their page with the question as 'Answered', they then may click and view the answer given.
+            `,
+        showOff: true
+    },
+    {
+        title: "Spirit Animals",
+        shortDesc: "MMORPG",
+        thumbnail: "",
+        librariesUsed: "Unity, Mirror (Unity Networking Tool), C#, Blender",
+        longDesc: `
+            This is a private repo, in order to view the code, you must ask for permission by contacting me. <br/><br/>
+            Spirit Animals is an MMORPG which I am currently creating, built in Unity, this game has been built from the ground up, this includes, planning out a brief overview of the game, objective and story line, using best practises whilst keeping in mind to prevent circle of dependencies, implementing movement of the character, including animation for evolution, inventory system, drop and pick up item system, system settings, stats for characters, attacking, talking with NPCs, Questing and much more!
+        `,
+        showOff: true
+    },
+    {
+        title: "Various work games",
+        shortDesc: "Brief summary of games I have created during work, though cannot disclose code or pictures.",
+        thumbnail: "",
+        librariesUsed: "CreateJS, Unity, PaperJS, Vanilla JavaScript, Speechelo",
+        longDesc: `
+            <em>Because of copyright, I am unable to provide pictures or code for these games, but wanted to write a brief list on the variety of games I have created and proud of.</em> <br/><br/>
+            <ul class="smallText">
+                <li>Fireworks: This game has three fireworks that shoot up from the bottom of window.innerHeight, they are all affected by 'gravity' with a constant pull to the floor, and are spawned with random speed values, so they all finish their path at different points on the screen. Once the firework has reached the end of its path, it will explode and create coloured particles (the colour is picked at random, at spawn of firework), with a random number between a min/max value, and there is a small chance that it will spawn a multicoloured firework. The spread of the particles once they are spawned is also random, giving the image of 'big' and 'small' fireworks.</li><br/>
+                <li>Puzzle: This game uses its own shape for the puzzle pieces using a custom path, depending on whether it is an edge piece or not, it will appropriately give a flat side to either one or two sides (depending if edge or corner piece). Once the grid has been made up, we overlay an image which is cut up accordingly to the puzzle piece. When the puzzle piece is interacted with (hovered over), the puzzle piece will move to it's place on the grid, and the next available puzzle pieces will be spawned in (note that the puzzle pieces will only spawn in if they are the next available pieces), once the puzzle is complete, the puzzle fades out and the original image fades in.</li><br/>
+                <li>Quiz: A quiz game with a pool of questions, this randomly selects questions and populates the quiz whilst playing, when choosing a potential answer, it uses an external library which specialises in text to voice (Speechelo).</li><br/>
+                <li>Football: Simple game made within Unity, making use of invisible borders (so the football cannot move outside of the pitch), a fixed camera of the pitch, the mouse controls a 'pointer' in game which them interacts with the ball, affected by gravity as it has a rigidbody and a collider. Two goals either side of the pitch which both have collission triggers attached and triggers a sound and an image to appear when a goal has been made. UI Scoreboard updates when goals are made and sounds play when the ball is hit and when the player can start playing the game (starting whitle).</li><br/>
+                <li>And more!</li><br/>
+            </ul>
+        `,
+        showOff: true
+    },
+    {
+        title: "Exercise Trainer App",
+        shortDesc: "App that allows you to create timed unlimited exercises within a 'Set'",
+        thumbnail: "",
+        librariesUsed: "Ionic, Android Studio",
+        longDesc: `
+            <p>
+                The idea of creating this app was born from starting to run in my personal time, I needed an app which allowed me to do timed intervals of walking, running and sprinting. I found an incredible app on Google Play which allowed me to do this, however was hugely limited as I was only able to add a certain number of exercises per set before I had to pay for their subscription.
+                I felt this was a complete ripoff, so used this opportunity to start creating my own App.<br/>
+                With this app, you can create as many sets (which include as many exercises as you want within each set) as you like, you have the option of counting in either seconds or minutes (for the ease of saying 10 seconds vs 60 seconds) and when you start the countdown, gives a nice big timer with text to say how long you have left of this exercise, what exercise you are currently doing, what set you are currently on, and what is the total time left of all the set and exercises.<br/>
+                Though this app was inteded for exercise, you could use this in multiple situations, as it is quite simply, an advanced timer application. <br/>
+                Through personal use, I have found improvements that need to be made, such as adding a sound when the exercise changes (in this example, I was running and didn't notice visually that the exercise had changed to walk, and vice versa, without keeping a hawk eye on the app at all times), therefore this app is still a work-in-progress however is at the point of being Version 1.
+I do have a standalone .apk which I have made within Android Studio (using the ionic API capacitor), so can pass this on through email or shared link, it is my hope in the near future I may publish this on Google Play so others can enjoy the benefit this app can bring. <br/> 
+            </p>
+        `,
+        showOff: true
     }
 ]
 
@@ -53,8 +110,8 @@ function SetupLoader() {
             SpawnDandelion(seed, x, y);
             const randomX = GenerateRandomNumber(maxDandelionXPosSpawn);
             const randomY = GenerateRandomNumber(maxDandelionYPosSpawn - seed.height);
-            x += randomX;
-            y += randomY;
+            x = randomX;
+            y = randomY;
         })
     })
 }
@@ -111,8 +168,8 @@ function SpawnDandelion(_dandelion, xPos, yPos) {
         _dandelion.read = true;
         Fade(_dandelion, 0.5);
         Blur(_dandelion, 3);
-        PopulateGUI(_dandelion);
-        OpenGUI("project");
+        PopulateGUI(_dandelion, "project");
+        OpenGUI();
         _dandelion.userHovering = false;
 
     })
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -16,17 +16,15 @@ const blurFilter1 = new PIXI.filters.BlurFilter();
 
 //GUI
 let guiOpen = false;
-let guiType = "";
 const overlay = document.querySelector("#overlay");
 const closeButton = document.querySelector("#closeButton");
 closeButton.addEventListener("click", () => {
-    CloseGUI(guiType);
+    CloseGUI();
 })
-const legendContent = document.querySelector("#legendContent");
-const projectContent = document.querySelector("#projectContent");
+const content = document.querySelector("#content");
 const title = document.querySelector("#title");
 const libariesUsed = document.querySelector("#librariesUsed");
-const thumbnail = document.querySelector("#thumbnail");
+let thumbnail = document.querySelector("#thumbnail");
 const shortDesc = document.querySelector("#shortDesc");
 const longDesc = document.querySelector("#longDesc");
 
@@ -34,7 +32,72 @@ const longDesc = document.querySelector("#longDesc");
 //SYMBOL BUTTON
 const legendButton = document.querySelector("#legend");
 legendButton.addEventListener("click", () => {
-  OpenGUI("legend");
+    PopulateGUI(
+        {
+            title: "Symbols",
+            thumbnail: "img/dandelionSeed.png",
+            longDesc: "Look out for the Dandelion Seed, click them to see different projects."
+        },
+        "legend"
+    )
+    OpenGUI();
+})
+
+//ABOUT BUTTON
+const aboutButton = document.querySelector("#aboutMe");
+aboutButton.addEventListener("click", () => {
+    PopulateGUI(
+        {
+            title: "Short and Sweet",
+            thumbnail: "",
+            longDesc: `
+                <div id="about">
+                    <p>
+                    <p class="heading">Welcome, to my portfolio!<br/><br/></p>
+    
+                    I hope you are well at this current time.<br/><br/>
+    
+                    I am a frontend / backend / games developer with a passion for learning, improving oneself and a Linux enthusiast.<br/><br/>
+    
+                    <em><u>Because I am a huge fan of mindfulness and meditation, my portfolio is themed around this, though I encourage everyone to practise meditation and mindfulness, I appreciate you may not have the time to take to see my portfolio in this light, therefore I have added a "Short on time?" mode, please feel free to use it. I hope you like the uniqueness.</u></em>
+                  </p>
+                  <p class="heading">What technologies have I worked with (To name a few...)?</p>
+                  <p>
+                    <ul>
+                      Frontend:
+                      <li>  React, Vue, Typescript, Bootstrap, P5, Ionic and more!</li>
+    
+                      Backend:
+                      <li>  NodeJS, Electron, Express, C#, SQL, MongoDB and Arduino.</li>
+    
+                      Games:
+                      <li>Phaser, CreateJS, PixiJS and Unity.</li>
+    
+                </ul>
+                  </p>
+                  <p class="heading">A bit about me</p>
+                  <p>
+                    First and foremost is I am eager to learn as much as I can, self improvement in both a personal and work sense highly appeals to me.
+                    My IT experience compliments my development career as it gives me a very rounded view on how technology works as a whole, allowing me to think outside the box.
+    
+                    I very much enjoy structure mixed with creativity, this is perfect within development as it allows me to come up with unique results and ideas.
+    
+                    In my spare time, I'm creating my own MMORPG game using Unity as the game engine, this has taught me a lot and the knowledge I have gained I am currently using within a work environment, it has excelled my learning experience because of my eargerness and urge to continously improve myself.
+    
+                    I also implement programming electronic solutions with a Raspberry Pi and Arduinos, my latest project is a replicating remote control.
+    
+                    Last thing you'll need to know about me, is I have a passion for Linux (Though will use all other OS's), this was born out of the years of Windows Updates randomly turning off my computer, and uninvited BSODs through the years.
+    
+                    I have found Linux to be not only reliable, but powerful for a power user, it's sped up so much of my workflow in my personal time.
+    
+                  </p>
+                </div>
+                
+            `
+        },
+        "about"
+    )
+    OpenGUI();
 })
 
 
@@ -182,37 +245,16 @@ function Fade(_dandelion, amount) {
     _dandelion.alpha = amount;
 }
 
-const CloseGUI = (subGUI) => {
+const CloseGUI = () => {
     //TODO Add fade out through CSS or JQuery
     overlay.classList.remove("show");
     overlay.classList.add("hide");
-    switch(subGUI) {
-        case "project":
-            projectContent.classList.remove("show");
-            projectContent.classList.add("hide");
-            break;
-        case "legend":
-            legendContent.classList.remove("show");
-            legendContent.classList.add("hide");
-            break;
-    }
 }
 
-const OpenGUI = (subGUI) => {
+const OpenGUI = () => {
     //TODO Add fade in through CSS or JQuery
     overlay.classList.remove("hide");
     overlay.classList.add("show");
-    switch(subGUI) {
-        case "project":
-            projectContent.classList.remove("hide");
-            projectContent.classList.add("show");
-            guiType = subGUI;
-            break;
-        case "legend":
-            legendContent.classList.remove("hide");
-            legendContent.classList.add("show");
-            break;
-    }
 }
 
 const CheckIfGuiOpen = () => {
@@ -229,10 +271,35 @@ const CheckIfGuiOpen = () => {
     })
 }
 
-const PopulateGUI = (dandelion) => {
-    title.textContent = dandelion.project.Title;
-    libariesUsed.textContent = dandelion.project.LibrariesUsed;
-    thumbnail.src = dandelion.project.Thumbnail;
-    shortDesc.textContent = dandelion.project.ShortDesc;
-    longDesc.textContent = dandelion.project.LongDesc;
+const PopulateGUI = (_object, type) => {
+    switch(type) {
+        case "project":
+            // thumbnail.classList.remove("hide");
+            // thumbnail.classList.add("show");
+            title.textContent = _object.project.title;
+            libariesUsed.textContent = _object.project.librariesUsed;
+            thumbnail.src = _object.project.thumbnail;
+            shortDesc.textContent = _object.project.shortDesc;
+            longDesc.innerHTML = _object.project.longDesc;
+            break;
+        case "legend":
+            // thumbnail.classList.remove("hide");
+            // thumbnail.classList.add("show");
+            title.innerHTML = `<span class="legend">${_object.title}</span>`;
+            libariesUsed.textContent = "";
+            thumbnail.src = _object.thumbnail;
+            shortDesc.textContent = "";
+            longDesc.innerHTML = `<span class="legend">${_object.longDesc}</span>`;
+            break;
+        case "about":
+            title.innerHTML = `${_object.title}`;
+            libariesUsed.textContent = "";
+            thumbnail.src = _object.thumbnail;
+            // thumbnail.classList.remove("show");
+            // thumbnail.classList.add("hide");
+            shortDesc.textContent = "";
+            longDesc.innerHTML = `${_object.longDesc}`;
+            break;
+    }
+
 }
