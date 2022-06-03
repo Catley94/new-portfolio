cf8f855013acb3f812f49416bd41dd5a15e319a3
positive-season <scp94373@gmail.com>
2022-06-03 14:49:22 +0100

    GUI opens and populates data

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ const projects = [
         Title: "Text adventure template",
         ShortDesc: "Template for creating text adventure games",
         Thumbnail: "",
-        LibrariesUsed: "P5.JS",
+        LibrariesUsed: "",
         LongDesc: "Text adventure template is exactly that, a template that allows others to edit and create their own two option text adventure game. This was originally created for covid lockdown for creativity, it's made for simplicity, everything is dynamic, all you need to do is add the state onto the storyBook object and the rest will happen like magic!"
     }
 ]
@@ -34,10 +34,8 @@ const maxDandelions = projects.length;
 /*
     TODO
     Each dandelion to have different speeds based on size (bigger is slower)
-    When completely off screen, move to left side of screen
-    Dandelions also float up and down
-    Create GUI for displaying projects
-    Stop dandelion when hovering over and display GUI
+    BUG: When hovering over the dandelion for some time, dandelion increase YSpeed dramtically
+    Add Thumbnail images
 */
 
 // Initialise Functions
@@ -89,6 +87,12 @@ function AddToDandelionsArray(_image) {
 }
 
 function SpawnDandelion(_dandelion, xPos, yPos) {
+    //Private functions
+    function _AssignProject(item) {
+        item.project = projects[item.id];
+        console.log(item.project);
+    }
+    _dandelion.id = dandelions.length;
     _dandelion.scale.x = scale;
     _dandelion.scale.y = scale;
     _dandelion.xSpeed = 0;
@@ -97,33 +101,39 @@ function SpawnDandelion(_dandelion, xPos, yPos) {
     _dandelion.originalYPos = yPos;
     _dandelion.x = _dandelion.originalXPos;
     _dandelion.y = _dandelion.originalYPos;
+    _dandelion.read = false;
     _dandelion.userHovering = false;
     _dandelion.interactive = true;
+    _AssignProject(_dandelion);
+
     _dandelion.on("pointerover", () => {
         _dandelion.userHovering = true;
-        //TODO Add filter colour when read, so user knows they have already read this project
+    })
+
+    _dandelion.on("pointerdown", () => {
+        _dandelion.read = true;
+        console.log("dandelion read: ", _dandelion.id);
+        PopulateGUI(_dandelion);
+        OpenGUI();
+        _dandelion.userHovering = false;
+        //TODO Add filter colour when read, so user knows they have already read this project OR add transparency
         //TODO Add GUI to appear when hovering
     })
-    _dandelion.on("pointerout", () => {_dandelion.userHovering = false;})
+
+    _dandelion.on("pointerout", () => {
+        _dandelion.userHovering = false;
+    })
+
+    //Changes cursor to hand when interactive events trigger
     _dandelion.buttonMode = true;
+
     AddToContainer(dandelionContainer, _dandelion);
     AddToDandelionsArray(_dandelion);
 }
 
-function CheckIfExceedsBounds(dandelion) {
-    // Two dandelion's width worth off screen, move Dandelion to left side of screen.
-    if (dandelion.x - dandelion.width >= window.innerWidth) {
-        // TODO Move dandelion to left side of screen;
-        dandelion.x = GenerateRandomNumber(maxDandelionXPosSpawn);
-        console.log("Resetting XPOS")
-    }
-    if (dandelion.y + dandelion.height >= window.innerHeight || dandelion.y < 0 && dandelion.x + dandelion.with >= window.innerWidth) {
-        // TODO Move dandelion to left side of screen;
-    }
-}
 
 app.ticker.add((delta) => {
-
+    CheckIfGuiOpen();
     if(dandelions) {
         dandelions.forEach((dandelion) => {
             CheckDandelionSpeed(dandelion);
@@ -131,7 +141,6 @@ app.ticker.add((delta) => {
             !dandelion.userHovering ? MoveDandelion(dandelion) : null;
         })
     }
-
 });
 
 SetupLoader();
---
+++ assets/js/utils.js
@@ -0,0 +1,139 @@
+let scale = (window.innerHeight / window.innerWidth) * 0.5;
+
+//GUI
+let guiOpen = false;
+const projectView = document.querySelector("#projectView");
+const closeButton = document.querySelector("#closeButton");
+closeButton.addEventListener("click", () => {
+    CloseGUI();
+})
+const content = document.querySelector("#content");
+const title = document.querySelector("#title");
+const libariesUsed = document.querySelector("#librariesUsed");
+const thumbnail = document.querySelector("#thumbnail");
+const shortDesc = document.querySelector("#shortDesc");
+const longDesc = document.querySelector("#longDesc");
+
+function SetupPixiStage() {
+    return new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+}
+
+function SetupPixiStageRenderer() {
+    return new PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, { antialias: true });
+}
+
+function AddContainerToStage(container) {
+    app.stage.addChild(container);
+}
+
+function AddStageToHTML() {
+    document.body.appendChild(app.view);
+}
+
+function AddToContainer(container, item) {
+    container.addChild(item);
+}
+
+
+const AssignWindSpeed = (_dandelion, _speed) => {
+    // TODO ASSIGN TO xSpeed, NOT speed
+    _dandelion.xSpeed = _speed;
+}
+
+const StartFloating = (_dandelion) => {
+    function _MoveYPosition(_dandelion) {
+        _dandelion.y += _dandelion.ySpeed;
+    }
+
+    _MoveYPosition(_dandelion);
+
+}
+
+const incrementSpeed = (item, _amount) => {
+    item.ySpeed += _amount;
+}
+
+const decrementSpeed = (item, _amount) => {
+    item.ySpeed -= _amount;
+}
+
+
+function CheckDandelionSpeed(dandelion) {
+    if (dandelion.xSpeed === 0) {
+        const windSpeed = GenerateRandomWindSpeed();
+        AssignWindSpeed(dandelion, windSpeed);
+    }
+
+    if(dandelion.y - dandelion.originalYPos > 50) {
+        decrementSpeed(dandelion, floatingSpeed);
+    } else {
+        incrementSpeed(dandelion, floatingSpeed);
+    }
+}
+
+
+function MoveDandelion(dandelion) {
+    function _MoveXPosition() {
+        dandelion.x += dandelion.xSpeed;
+    }
+
+    _MoveXPosition();
+    StartFloating(dandelion);
+}
+
+function GenerateRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function GenerateRandomWindSpeed() {
+    return GenerateRandomNumber(maxWindSpeed);
+}
+
+function CheckIfExceedsBounds(dandelion) {
+    const dandelionXPosOffScreen = dandelion.x - dandelion.width >= window.innerWidth;
+    const dandelionYPosOffScreen = dandelion.y - (dandelion.height) * 0.5 > window.innerHeight || dandelion.y + (dandelion.height) * 0.5 < 0;
+    // Two dandelion's width worth off screen, move Dandelion to left side of screen.
+    if (dandelionXPosOffScreen) {
+        dandelion.x = GenerateRandomNumber(maxDandelionXPosSpawn);
+    }
+    // One and half dandelion's height worth off screen (both top and bottom of screen),
+    // And
+    // move Dandelion to left side of screen.
+    if (dandelionYPosOffScreen && !dandelionXPosOffScreen) {
+        dandelion.x = GenerateRandomNumber(maxDandelionXPosSpawn);
+    }
+}
+
+const CloseGUI = () => {
+    //TODO Add fade out through CSS or JQuery
+    projectView.classList.remove("show");
+    projectView.classList.add("hide");
+}
+
+const OpenGUI = () => {
+    //TODO Add fade in through CSS or JQuery
+    projectView.classList.remove("hide");
+    projectView.classList.add("show");
+}
+
+const CheckIfGuiOpen = () => {
+    projectView.classList.forEach((className) => {
+        if(className === "hide") {
+            if(guiOpen) {
+                guiOpen = false;
+            }
+        } else if(className === "show") {
+            if(!guiOpen) {
+                guiOpen = true;
+            }
+        }
+    })
+}
+
+const PopulateGUI = (dandelion) => {
+    title.textContent = dandelion.project.Title;
+    libariesUsed.textContent = dandelion.project.LibrariesUsed;
+    thumbnail.src = dandelion.project.Thumbnail;
+    shortDesc.textContent = dandelion.project.ShortDesc;
+    longDesc.textContent = dandelion.project.LongDesc;
+}
