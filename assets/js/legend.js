7291b5c8af5d8fe99f3de3f4ba55955363391961
positive-season <scp94373@gmail.com>
2022-06-04 16:04:11 +0100

    Added email

--- assets/js/legend.js
+++ assets/js/legend.js
@@ -5,6 +5,7 @@ const legend = {
         <ul>
         <li>Look out for the Dandelion Seed, click them to see different projects.<br/></li>
         <li>Click on "Short on time?" to move all Dandelion Seeds in the canvas, they will bounce around instead of float from one side of the screen to the other.</li>
+        <li>I would love feedback, please contact me at: <a id="email" href="mailto:s.catley@live.co.uk">s.catley@live.co.uk</a></li>
         </ul>
     `
 }
