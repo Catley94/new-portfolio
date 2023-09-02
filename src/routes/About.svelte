dea77812962c5428318234159263db49b6c26ca0
positive-season <scp94373@gmail.com>
2023-09-02 22:12:27 +0100

    Added test video and logos

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -8,12 +8,32 @@
             image: "/Logos/unity-engine.png",
         },
         {
+            name: "C#",
+            image: "/Logos/csharp-logo.png",
+        },
+        {
+            name: "HTML",
+            image: "/Logos/html-logo.png",
+        },
+        {
+            name: "CSS",
+            image: "/Logos/css-logo.png",
+        },
+        {
             name: "JavaScript",
             image: "/Logos/js-logo.png",
         },
         {
             name: "Svelte",
             image: "/Logos/svelte-logo.png",
+        },
+        {
+            name: "React",
+            image: "/Logos/react-logo.png",
+        },
+        {
+            name: "NodeJS",
+            image: "/Logos/node-logo.png",
         },
         {
             name: "Tailwind CSS",
@@ -42,7 +62,7 @@
     <PrimaryHeader header="Technolgies"/>
     <div class="flex flex-wrap justify-center">
         {#each technologies as technology, i (i)}
-            <img class="w-1/6 m-5" src={technology.image} alt={technology.name + " Logo"}/>
+            <img class="object-contain w-14 md:w-16 m-5 hover:drop-shadow-lg" src={technology.image} alt={technology.name + " Logo"}/>
         {/each}
     </div>
 </div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,8 +19,10 @@
 <!--    <img src="/verne-ho-0LAJfSNa-xQ-unsplash.jpg" alt="TODO: FILL ME IN" />-->
 <!--    <h1 class="uppercase text-center my-5 text-3xl font-bold">Samuel Catley</h1>-->
     <div
-            class="relative overflow-hidden bg-cover bg-no-repeat p-12 text-center"
-            style="background-image: url('/verne-ho-0LAJfSNa-xQ-unsplash.jpg'); height: 400px">
+            class="relative overflow-hidden bg-cover bg-no-repeat text-center">
+        <video poster="/" src="/Videos/2023-09-02 21-57-26.mp4" loop autoplay>
+            <track kind="captions" />
+        </video>
         <div
                 class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
                 style="background-color: rgba(0, 0, 0, 0.6)">
